b726ca5c7c0c7e3115e783f7ee978dc9e9aea9cd
sid2417 <siddhartha.java9398@gmail.com>
2024-12-31 20:57:13 +0530

    variables practice

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VARIABLE1=$1
-VARIABLE2=$2
+echo "Please Enter Your UserName :: "
 
-echo "$1:: Hi $2, How are you"
-echo "$2:: Hi $1, I am good, What about you"
-echo "$1:: Yes $2, I am doing well, and how was the day"
-echo "$2:: Fine $1, its going well.."
+read USERNAME   # Here USERNAME is variable
+
+echo "Please Enter Your Password :: "
+
+read PASSWORD    # Here PASSWORD is variable
