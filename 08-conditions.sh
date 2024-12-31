4f9c7919b2dd666937fb4dca7befa96f526c3091
sid2417 <siddhartha.java9398@gmail.com>
2024-12-31 21:57:55 +0530

    variables practice

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -7,6 +7,18 @@ then
     echo "You have sudo access..."
 else    
     echo "Please Provide Sudo Access..."
+    exit 2
 fi
+
+
+dnf install nginx -y
+
+if [ $? -eq 0 ]
+then
+    echo "Your installation is going Success.."
+else
+    echo "Your installation is going Error.."
+fi
+
 
 echo "This is my script.."
