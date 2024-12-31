1ae572d89bf264dde2f93afa2ca90ba7252ae15d
sid2417 <siddhartha.java9398@gmail.com>
2024-12-31 20:58:15 +0530

    variables practice

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -s "Please Enter Your UserName :: "
+
+read USERNAME   # Here USERNAME is variable
+
+echo -s "Please Enter Your Password :: "
+
+read PASSWORD    # Here PASSWORD is variable
