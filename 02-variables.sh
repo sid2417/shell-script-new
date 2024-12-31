3d0831ca0a8fe651700dc4f8216b4123c4df2e7d
sid2417 <siddhartha.java9398@gmail.com>
2024-12-31 20:41:20 +0530

    variables practice

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VARIABLE1=Ramesh
+VARIABLE2=Suresh
+
+echo "$VARIABLE1:: Hi Suresh, How are you"
+echo "Suresh:: Hi Ramesh, I am good, What about you"
+echo "Ramesh:: Yes Suresh, I am doing well, and how was the day"
+echo "Suresh:: Fine Ramesh, its going well.."
