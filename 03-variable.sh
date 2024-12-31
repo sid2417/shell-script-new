49c71259f4328c72633b34f29404b98c45805035
sid2417 <siddhartha.java9398@gmail.com>
2024-12-31 20:52:39 +0530

    variables practice

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 #Passing Arguments through the command prompt
+
 VARIABLE1=$1
 VARIABLE2=$2
+
+
+
+echo "$VARIABLE1:: Hi $VARIABLE2, How are you"
+echo "$VARIABLE2:: Hi $VARIABLE1, I am good, What about you"
+echo "$VARIABLE1:: Yes $VARIABLE2, I am doing well, and how was the day"
+echo "$VARIABLE2:: Fine $VARIABLE1, its going well.."
 
 echo "$1:: Hi $2, How are you"
 echo "$2:: Hi $1, I am good, What about you"
 echo "$1:: Yes $2, I am doing well, and how was the day"
-echo "$2:: Fine $2, its going well.."
+echo "$2:: Fine $1, its going well.."
---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VARIABLE1=$1
+VARIABLE2=$2
+
+echo "$1:: Hi $2, How are you"
+echo "$2:: Hi $1, I am good, What about you"
+echo "$1:: Yes $2, I am doing well, and how was the day"
+echo "$2:: Fine $1, its going well.."
