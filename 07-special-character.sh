a895adc6585c5160a85564cb39866b31b943d6ad
sid2417 <siddhartha.java9398@gmail.com>
2024-12-31 21:27:29 +0530

    variables practice

---
+++ 07-special-character.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VARIABLE1=20
+VARIABLE2=David
+
+echo "Total varaiables list :: $@"
+
+echo "you need total how many variables used in this script :: $#"
+
+echo "your hostname :: $HOSTNAME"
+
+echo "your current directory :: $PWD"
+
+echo "which user using this :: $USER"
+
+echo "process instance id :: "
