ee33296e2f252445a51160f1531c0ea4d9c700f3
sid2417 <siddhartha.java9398@gmail.com>
2025-01-07 00:54:25 +0530

    forloop

--- 09-install-packages.sh
+++ 09-install-packages.sh
@@ -7,28 +7,38 @@
 # 4. Need colors
 # 5. install packages in loop manner
 
+Y="\e[30m"
+R="\e[31m"
+G="\e[32m"
+B="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date+%F-%H-%M-%S)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+
 
 USERID=$(id -u)
 if [ USERID -nq 0 ]
 then   
-    echo "You Have already SUDO access..."
+    echo -e $G"You Have already SUDO access..."$N
 else    
-    echo "Please Provide ROOT access.."
+    echo -e $Y "Please Provide ROOT access.." $N
 fi
 
-for packages in {$@}
+
+for packages in $@
 do
-    echo $packages
+    echo -e $Y"******$packages*******"$N &>>$LOGFILE
+    dnf list installed $packages 
+    if [ $? -ne 0 ]
+    then
+        dnf install $packages -y &>>$LOGFILE
+    else    
+        echo -e $G"$packages is already installed in your server"$N &>>$LOGFILE
+    fi
 done
 
-
-dnf list installed git
-if [ $? -nq 0 ]
-then
-    dnf install git -y
-else    
-    echo "Git is already installed in your server"
-fi
 
 
 
