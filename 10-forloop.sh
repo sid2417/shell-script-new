79e65b773cb13e88183518f385d1cca1eacbde13
sid2417 <siddhartha.java9398@gmail.com>
2025-01-07 00:28:34 +0530

    forloop

--- 10-forloop.sh
+++ 10-forloop.sh
@@ -1,6 +1,13 @@
 for packages in {$@}
 do
     echo $packages
+    dnf list installed $packages
+    if [ $? -nq 0 ]
+    then
+        dnf install $packages -y
+    else    
+        echo "$packages is already installed in your server"
+    fi
 done
 
 
